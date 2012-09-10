fcd36f83524e4daa0115edc37ca33d6fc2bc8698
Charles Leifer <coleifer@gmail.com>
2012-09-10 13:32:26 -0500

    Support generic file-like objects

--- beefish.py
+++ beefish.py
@@ -5,12 +5,19 @@ import sys
 import unittest
 from random import randrange
 
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
 from Crypto.Cipher import Blowfish
 from Crypto import Random
 
 
 def _gen_padding(fh):
-    buflen = os.fstat(fh.fileno()).st_size
+    fh.seek(0, 2)
+    buflen = fh.tell()
+    fh.seek(0)
     pad_bytes = 8 - (buflen % 8)
     padding = Random.get_random_bytes(pad_bytes - 1)
     bflag = randrange(6, 248)
@@ -20,51 +27,49 @@ def _gen_padding(fh):
 def _read_padding(buffer):
     return (ord(buffer[-1]) % 8) or 8
 
-def encrypt(in_file, out_file, key, chunk_size=4096):
-    in_fh = open(in_file, 'rb')
-    out_fh = open(out_file, 'wb')
-
-    padding = _gen_padding(in_fh)
+def encrypt(in_buf, out_buf, key, chunk_size=4096):
+    padding = _gen_padding(in_buf)
     cipher = Blowfish.new(key, Blowfish.MODE_ECB)
     wrote_padding = False
 
     while 1:
-        buffer = in_fh.read(chunk_size)
+        buffer = in_buf.read(chunk_size)
         if buffer:
             if len(buffer) < chunk_size:
                 buffer += padding
                 wrote_padding = True
-            out_fh.write(cipher.encrypt(buffer))
+            out_buf.write(cipher.encrypt(buffer))
         else:
             if not wrote_padding:
-                out_fh.write(cipher.encrypt(padding))
+                out_buf.write(cipher.encrypt(padding))
             break
 
-    in_fh.close()
-    out_fh.close()
-
-def decrypt(in_file, out_file, key, chunk_size=4096):
-    in_fh = open(in_file, 'rb')
-    out_fh = open(out_file, 'wb')
-    
+def decrypt(in_buf, out_buf, key, chunk_size=4096):
     cipher = Blowfish.new(key, Blowfish.MODE_ECB)
     decrypted = ''
     
     while 1:
-        buffer = in_fh.read(chunk_size)
+        buffer = in_buf.read(chunk_size)
         if buffer:
             decrypted = cipher.decrypt(buffer)
-            out_fh.write(decrypted)
+            out_buf.write(decrypted)
         else:
             break
 
     if decrypted:
         padding = _read_padding(decrypted)
-        out_fh.seek(-padding, 2)
-        out_fh.truncate()
+        out_buf.seek(-padding, 2)
+        out_buf.truncate()
 
-    out_fh.close()
-    in_fh.close()
+def encrypt_file(in_file, out_file, key, chunk_size=4096):
+    with open(in_file, 'rb') as in_fh:
+        with open(out_file, 'wb') as out_fh:
+            encrypt(in_fh, out_fh, key, chunk_size)
+
+def decrypt_file(in_file, out_file, key, chunk_size=4096):
+    with open(in_file, 'rb') as in_fh:
+        with open(out_file, 'wb') as out_fh:
+            decrypt(in_fh, out_fh, key, chunk_size)
 
 class TestEncryptDecrypt(unittest.TestCase):
     def setUp(self):
@@ -96,8 +101,8 @@ class TestEncryptDecrypt(unittest.TestCase):
         out_key = out_key or self.key
 
         buf = self.write_bytes(num_bytes, ch)
-        encrypt(self.in_filename, self.out_filename, in_key, chunk_size)
-        decrypt(self.out_filename, self.dec_filename, out_key, chunk_size)
+        encrypt_file(self.in_filename, self.out_filename, in_key, chunk_size)
+        decrypt_file(self.out_filename, self.dec_filename, out_key, chunk_size)
 
         with open(self.dec_filename, 'rb') as fh:
             decrypted = fh.read()
@@ -125,6 +130,19 @@ class TestEncryptDecrypt(unittest.TestCase):
             for num_bytes in nb:
                 buf, decrypted = self.crypt_data(num_bytes, 'a', chunk_size=i)
                 self.assertEqual(buf, decrypted)
+
+    def test_stringio(self):
+        for i in [128, 1024, 2048, 4096]:
+            nb = [i - 1, i, i + 1, i * 2, i * 2 + 1]
+            for num_bytes in nb:
+                in_buf = StringIO()
+                out_buf = StringIO()
+                dec_buf = StringIO()
+                in_buf.write(num_bytes * 'a')
+                encrypt(in_buf, out_buf, self.key, i)
+                out_buf.seek(0)
+                decrypt(out_buf, dec_buf, self.key, i)
+                self.assertEqual(in_buf.getvalue(), dec_buf.getvalue())
 
 
 if __name__ == '__main__':
@@ -161,6 +179,6 @@ if __name__ == '__main__':
             sys.exit(2)
 
     if options.encrypt:
-        encrypt(infile, outfile, key)
+        encrypt_file(infile, outfile, key)
     else:
-        decrypt(infile, outfile, key)
+        decrypt_file(infile, outfile, key)
