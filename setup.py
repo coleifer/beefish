8b70b3366a83119435df6f369baa772eb38bde4a
Charles Leifer <coleifer@gmail.com>
2014-04-11 06:15:35 -0500

    Version bamp

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='beefish',
-    version="0.1.1",
+    version="0.1.2",
     description='simple file encryption using pycrypto',
     long_description=readme,
     author='Charles Leifer',
